e0bd4c6a70c8df18d82301f38ca6e9dd34a9d78e
alstjr5949 <alstjr5949@gmail.com>
2025-05-25 00:51:15 +0900

    study: cursor composer

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">환영합니다</h1>
+      <p className="text-lg text-gray-600">
+        이 웹사이트에 오신 것을 환영합니다.
+      </p>
+    </div>
+  );
+}
